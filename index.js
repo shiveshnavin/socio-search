06da2128e20c2374c2658198eeff7428dbcdb26b
Shivesh Navin <shiveshnavin@gmail.com>
2024-04-07 17:04:20 +0530

    Search instagram

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ var express = require("express")
 var ApolloServer = require('@apollo/server').ApolloServer
 var expressMiddleware = require('@apollo/server/express4').expressMiddleware;
 var { ruruHTML } = require("ruru/server")
-var Linkedin = require('./modules/linkedin.js')
+var Linkedin = require('./modules/linkedin.js');
+const Instagram = require("./modules/instagram.js");
 
 var typeDefs = `#graphql
 type Query {
   search(
-    linkedin: LinkedInSearchParams,
+    linkedin: SearchParams,
+    instagram: SearchParams,
     skip: Int
   ): [User]
   linkedinUser(username: String!) : User
+  instagramUser(username: String!) : User
 }
 
 type User {
@@ -20,6 +23,11 @@ type User {
   image: String
   thumbnail: String
   location: String
+
+  igUserName: String
+  igUserId: String
+  igBio: String
+  igBasic: String
 
   linkedinDegree: String
   linkedinUrl: String
@@ -36,7 +44,7 @@ type LinkedinEducation {
   yearTo: String
 }
 
-input LinkedInSearchParams {
+input SearchParams {
   text: String!
   city: String
 }
@@ -44,22 +52,46 @@ input LinkedInSearchParams {
 
 var resolvers = {
   User: {
+    igBio: (parent, args, ctx, info) => {
+      if (parent.igUserName)
+        return Instagram.getUser(parent.igUserName, parent).then(u => u.igBio)
+    },
+    igBasic: (parent, args, ctx, info) => {
+      if (parent.igUserName)
+        return Instagram.getUser(parent.igUserName, parent).then(u => u.igBasic)
+    },
     thumbnail: (parent, args, ctx, info) => {
-      return parent.image || parent.thumbnail || Linkedin.linkedinImage(parent.linkedinUserName)
+      if (parent.igUserName != undefined) {
+        return parent.image || parent.thumbnail || Instagram.getUser(parent.igUserName, parent).then(u => u.image)
+      }
+      if (parent.linkedinUserName)
+        return parent.image || parent.thumbnail || Linkedin.linkedinImage(parent.linkedinUserName)
     },
     image: (parent, args, ctx, info) => {
-      return Linkedin.linkedinImage(parent.linkedinUserName)
+      if (parent.igUserName != undefined) {
+        return Instagram.getUser(parent.igUserName, parent).then(u => u.image)
+      }
+      if (parent.linkedinUserName)
+        Linkedin.linkedinImage(parent.linkedinUserName)
     },
     linkedinEducation: (parent, args, ctx, info) => {
-      return Linkedin.linkedinEducation(parent.linkedinId)
+      if (parent.linkedinId)
+        return Linkedin.linkedinEducation(parent.linkedinId)
+      else
+        return undefined
     }
   },
   Query: {
+    instagramUser: (parent, args, ctx, info) => Instagram.getUser(args.username),
     linkedinUser: (parent, args, ctx, info) => Linkedin.linkedinUser(args.username),
     search: (parent, args, ctx, info) => {
       if (args.linkedin) {
         let li = args.linkedin
         return Linkedin.search(li.text, li.city, args.skip)
+      }
+      if (args.instagram) {
+        let li = args.instagram
+        return Instagram.search(li.text)
       }
       return []
     }
---
+++ modules/instagram.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+
+function mapIGToUser(igu) {
+    return {
+        name: igu.full_name != "" ? igu.full_name : igu.username,
+        summary: igu.biography,
+        subtitle: igu.biography,
+        igBio: igu.biography,
+        image: igu.hd_profile_pic_url_info?.url || igu.profile_pic_url,
+        thumbnail: igu.hd_profile_pic_url_info?.url || igu.profile_pic_url,
+        igUserName: igu.username,
+        igUserId: igu.pk,
+        igBasic: `private=${igu.is_private != undefined ? igu.is_private : '-'}, media=${igu.media_count || '-'}, following=${igu.following_count || '-'}, follower=${igu.follower_count || '-'}`
+    }
+}
+
+module.exports = {
+    search(search) {
+        let url = `https://bots.semibit.in/semibit-media/instagram-bot/searchUser?username=${encodeURIComponent(search)}&tenant=birbankaa`
+        return axios.get(url).then(rep => rep.data.map(mapIGToUser))
+    },
+    async getUser(username, cachedUser) {
+        if (cachedUser?.summary) {
+            return cachedUser
+        }
+        let url = `https://bots.semibit.in/semibit-media/instagram-bot/getUser?username=${encodeURIComponent(username)}&tenant=birbankaa`
+        return axios.get(url).then(rep => (mapIGToUser(rep.data)))
+    }
+}
